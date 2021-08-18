5cf9becc2ca5485fc34cdc089fa9f3f0a3ff30af
Matheus Dias <diasmatheus05@gmail.com>
2021-08-18 16:08:46 -0300

    Criando herança de usuário e vendedor, criando controller de usuário e vendedor

---
+++ src/main/java/controller/ControllerVendedor.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author veste
+ */
+public class ControllerVendedor {
+//    private String numCaixa;
+    
+    public Boolean adicionarProduto(String venda, String produto) {
+        return false;
+    }
+    
+    public Boolean removerProduto(String venda, String produto) {
+        return false;
+    }
+    
+    public Boolean iniciarVenda(String venda) {
+        return false;
+    }
+    
+    public Boolean finalizarVenda(String venda) {
+        return false;
+    }
+    
+    public Boolean incluirDesconto(String venda, Float desconto) {
+        return false;
+    }
+    
+    public Boolean gerarRecibo(String venda) {
+        return false;
+    }
+    
+    public Boolean pesquisarProduto(String produto) {
+        return false;
+    }
+}
